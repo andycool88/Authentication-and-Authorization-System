60b48440cce2a69e4d6f7c3d3cb8e92bf46f9076
Emaye Andrew <g.emaye@live.com>
2023-12-01 14:08:01 +0100

    login routes added

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const createError = require("http-errors");
 const User = require("../Models/User.model");
 const { authSchema } = require("../helpers/validation_schema");
+const { signAccessToken } = require("../helpers/jwt_helper");
+const { create } = require("@hapi/joi/lib/ref");
+
+///      REGISTER ROUTE
+
+///AUTHOR: EMAYE ANDREW
+///DESC: THIS REGISTER AUTH ROUTE IS HIGHLY SECURED WITH JWT
 
 router.post("/register", async (req, res, next) => {
   console.log(req.body);
@@ -17,15 +24,38 @@ router.post("/register", async (req, res, next) => {
 
     const user = new User(result);
     const savedUser = await user.save();
-    res.send(savedUser);
+    const accessToken = await signAccessToken(savedUser.id);
+
+    res.send({ accessToken });
   } catch (error) {
     if (error.isJoi === true) error.status = 422;
     next(error);
   }
 });
 
+///       LOGIN ROUTE
+
+///AUTHOR: EMAYE ANDREW
+///DESC: THIS LOGIN AUTH ROUTE IS HIGHLY SECURED WITH JWT
+
 router.post("/login", async (req, res, next) => {
-  res.send("login route");
+  try {
+    const result = await authSchema.validateAsync(req.body);
+    const user = await User.findOne({ email: result.email });
+
+    if (!user) throw createError.NotFound("User not registered");
+
+    const isMatch = await user.isValidPassword(result.password);
+    if (!isMatch) throw createError.Unauthorized("Username/password not valid");
+
+    const accessToken = await signAccessToken(user.id);
+
+    res.send(accessToken);
+  } catch (error) {
+    if (error.isJoi === true)
+      return next(createError.BadRequest("Invalid Username/Password"));
+    next(error);
+  }
 });
 
 router.post("/refresh-token", async (req, res, next) => {
--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -5,16 +5,20 @@ module.exports = {
   signAccessToken: (userId) => {
     return new Promise((resolve, reject) => {
       const payload = {};
-      const secret = "some super secret";
+      const secret = process.env.ACCESS_TOKEN_SECRET;
 
       const options = {
         expiresIn: "1h",
-        issuer: "pickurpage.com",
+        issuer: "nativecoder.com",
         audience: userId,
       };
 
       JWT.sign(payload, secret, options, (err, token) => {
-        if (err) reject(err);
+        if (err) {
+          console.log(err.message);
+          //reject(err);
+          reject(createError.InternalServerError());
+        }
         resolve(token);
       });
     });
